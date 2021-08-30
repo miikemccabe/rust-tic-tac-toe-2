72a2d09b4cd60942e05eca8b9701f786051b4a81
Mike McCabe <mike.mccabe@missguided.com>
2021-08-30 11:53:56 +0100

    Clear the screen initially

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
 
     let mut g = game::Game::new();
 
+    print!("\x1B[2J\x1B[1;1H"); // Clears the terminal
     g.display();
     while g.get_status() == game::GameStatus::InProgress {
         g.play();
