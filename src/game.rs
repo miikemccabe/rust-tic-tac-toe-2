e6c9977268cd4142af05a129ea61a0124abb33f8
Mike McCabe <mike.mccabe@missguided.com>
2021-08-30 12:27:52 +0100

    Handle winning

--- src/game.rs
+++ src/game.rs
@@ -2,6 +2,17 @@ use crate::board::Board;
 use crate::player::Player;
 use crate::io::{get_choice, Command, Choice};
 
+const WINNING_COMBOS: [[(usize, usize); 3]; 8]  = [
+  [(0,0), (0,1), (0,2)],
+  [(1,0), (1,1), (1,2)],
+  [(2,0), (2,1), (2,2)],
+  [(0,0), (1,0), (2,0)],
+  [(0,1), (1,1), (2,1)],
+  [(0,2), (1,2), (2,2)],
+  [(0,0), (1,1), (2,2)],
+  [(2,0), (1,1), (0,2)],
+];
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum GameStatus {
   Won(Player),
@@ -62,8 +73,22 @@ impl Game {
         println!("{}", err);
       }
     }
+    if let Some(player) = self.find_winner() {
+      self.status = GameStatus::Won(*player);
+      self.handle_win();
+    }
     if self.board.is_full() {
       self.handle_draw();
+    }
+  }
+
+  fn handle_win(&self) {
+    if let GameStatus::Won(player) = self.status {
+      print!("\x1B[2J\x1B[1;1H"); // Clears the terminal
+      self.display();
+      println!();
+      println!("Player {} wins!", player);
+      println!();
     }
   }
 
@@ -95,5 +120,34 @@ impl Game {
       Player::Cross => Player::Naught,
       Player::Naught => Player::Cross
     }
+  }
+
+  pub fn find_winner(&self) -> Option<&Player> {
+    let mut winner: Option<&Player> = None;
+
+    for combo in WINNING_COMBOS {
+      // Get the player at the first cell of each winning combo
+      let player = self.board.get_cell((combo[0].0, combo[0].1));
+      // Loop over each cell of the 3 cell winning combo
+      for cell in combo {
+        // Get the value of the cell
+        let current = self.board.get_cell((cell.0, cell.1));
+        // If this cell matches the current player, we've got a potential winner
+        if current.is_some() && player.is_some() && current.unwrap() == player.unwrap() {
+          // Tentatively set this as the winner and continue checking the rest of the combo
+          winner = current.as_ref();
+        } else {
+          // If any of the cells in this combo don't match the player this is not a winning combo. Give up on it.
+          winner = None;
+          break;
+        }
+      }
+      if winner.is_some() {
+        // If we have a winner after checking every cell in a combo, then we have a winner!
+        break;
+      }
+    }
+
+    winner
   }
 }
